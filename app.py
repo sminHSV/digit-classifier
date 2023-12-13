83615744f203b99cbd91f044fd0b1ff53f0afb1d
sminHSV <skim141@crimson.ua.edu>
2023-12-13 16:53:13 -0600

    fixed doc page

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 
 SWAGGER_URL = '/doc'
-API_URL = '/static/swagger.json'
+API_URL = url_for('static', filename='swagger.json')
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
